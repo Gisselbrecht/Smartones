043ef492f2a792c74a8ba301f278db273b8e7744
Gisselbrecht <martizr777@gmail.com>
2017-07-19 19:55:15 +0300

    Add files via upload

---
+++ Server_AdvanceTurn.lua
@@ -0,0 +1,12 @@
+﻿function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrder)
+
+	if(order.proxyType == 'GameOrderAttackTransfer') then
+
+		if(result.ActualArmies.NumArmies == 1 and game.ServerGame.LatestTurnStanding.Territories[order.To].NumArmies.NumArmies == 1 and result.IsAttack) then
+
+			skipThisOrder(WL.ModOrderControl.Skip);
+                end
+
+        end
+				
+end
